32c6693e631acdf0f398f141ef1c54eba80fe449
William Jensen <william.riis93@gmail.com>
2022-08-26 09:46:13 +0930

    styling of filters in header

--- src/components/Header/YearRange/index.js
+++ src/components/Header/YearRange/index.js
@@ -6,11 +6,11 @@ const YearRange = ({ changeEvent, min, max, current }) => {
 
     return (
         <div className='year-range__container'>
-            <p className='year-range__title'>YEAR</p>
+            <p className='year-range__title'>YEAR: {current}</p>
             <div className='year-range__sub-container'>
                 <span>{min}</span>
-                <input type="range" min={min} max={max} onChange={(e) => changeEvent(e.target.value)}></input>
-                <span>{current}</span>
+                <input className='year-range__slider' type='range' min={min} max={max} onChange={(e) => changeEvent(e.target.value)}></input>
+                <span>{max}</span>
             </div>
         </div>
     )
